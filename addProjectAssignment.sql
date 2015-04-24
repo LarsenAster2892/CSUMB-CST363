e7a3030910cea9f86febcba08e921a92b7436c3b
ndevcsumb <ndevaughn@csumb.edu>
2015-04-23 21:33:38 -0700

    Create addProjectAssignment.sql
    Add entries to the project assignment table.

---
+++ addProjectAssignment.sql
@@ -0,0 +1,22 @@
+set echo off;
+set verify off;
+set heading off;
+
+prompt;
+prompt --- Add Project Assignment Form ---;
+prompt;
+prompt;
+
+accept vProjectAssignmentID prompt 'Please enter the project Assignment ID number: ';
+
+accept vEmployeeID prompt 'Please enter the project Assignment associated employee ID number: ';
+
+accept vProjectID prompt 'Please enter the project Assignment associated project ID number: ';
+
+INSERT INTO projectAssignment
+VALUES ('&vProjectAssignmentID', '&vEmployeeID', '&vProjectID');
+
+prompt The following record has been added:;
+prompt;
+
+select * FROM projectAssignment WHERE projectAssignID = '&vProjectAssignmentID';

b74a86b804ea51bdcbcc28a15641ae7096947ad0
ndevcsumb <ndevaughn@csumb.edu>
2015-04-23 21:34:33 -0700

    Create addDepartment.sql
    Add entries to the department table.

---
+++ addDepartment.sql
@@ -0,0 +1,23 @@
+set echo off;
+set verify off;
+set heading off;
+
+prompt;
+prompt --- Add Department Form ---;
+prompt;
+prompt;
+
+accept vDeptID prompt 'Please enter the Department ID number: ';
+
+accept vDeptName prompt 'Please enter the Department name: ';
+
+accept vDeptManagerID prompt 'Please enter the Department manager employee ID number: ';
+
+
+INSERT INTO Department
+VALUES ('&vDeptID', '&vDeptName', '&vDeptManager');
+
+prompt The following record has been added:;
+prompt;
+
+select * FROM Department WHERE deptID = '&vDeptID';
